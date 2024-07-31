9588d2b897d32cf2399af15462372da0d0dc2962
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-07-31 12:18:21 +0600

    update json

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -2,9 +2,9 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const { User } = require("../models/User.model");
-require("dotenv").config();
-const secretKey = process.env.SECRETE_KEY;
+dotenv.config();
 
+const secretKey = process.env.SECRETE_KEY;
 
 const UserRegister = async (req, res) => {
   const { username, email, password, time, gender } = req.body;
@@ -24,37 +24,40 @@ const UserRegister = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "Registration successful" });
   } catch (error) {
-    res.status(500).json({ message: "Registration failed" });
+    res.status(500).json({ message: "Registration failed", error: error.message });
   }
 };
 
 const UserLogin = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
 
-  if (!user) {
-    return res.status(401).json({ message: "Invalid email or password" });
-  }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
 
-  if (await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({ userId: user._id }, secretKey, {
-      expiresIn: "5h",
-    });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
+    const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: "5h" });
+    res.json({ token, username: user.username });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error: error.message });
   }
 };
 
-const UserBookings = (req, res) => {
-  User.find({ _id: req.userId })
-    .populate("booking")
-    .then((users) => {
-      res.status(200).json(users);
-    })
-    .catch((err) => {
-      res.status(400).json({ error: "Unable to retrieve users " });
-    });
+const UserBookings = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).populate("booking");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to retrieve user bookings", message: error.message });
+  }
 };
 
-module.exports = {UserRegister, UserLogin, UserBookings};
+module.exports = { UserRegister, UserLogin, UserBookings };
